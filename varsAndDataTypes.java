c58383b7e30572b16f4dc4ff1d06d1afccc1174e
Proton <electronicballs69@gmail.com>
2025-08-26 13:37:45 -0500

    Update varsAndDataTypes.java

--- varsAndDataTypes.java
+++ varsAndDataTypes.java
@@ -1,5 +1,11 @@
-public class varsAndDataTypes.java {
+public class varsAndDataTypes {
   public static void main(String[] args) {
-    
+    float coffeePrice = 2.50;
+    float pastryPrice = 3.00;
+
+    int numberOfCoffees = 2;
+    int numberOfPastries = 1;
+
+    double subtotal = (coffeePrice * numberOfCoffees) + (pastryPrice * numberOfPastries)
   }
 }
