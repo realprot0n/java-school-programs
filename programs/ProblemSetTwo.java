1d9ec78bbc5d7b79b9b4050dd5ec04bf4a9d3dff
RealProt0n <electronicballs69@gmail.com>
2025-09-09 08:50:25 -0500

    fuh

---
+++ programs/ProblemSetTwo.java
@@ -0,0 +1,27 @@
+public class ProblemSetTwo {
+  public static void println(Object obj) {
+    System.out.println(obj);
+  }
+
+  public static String getMiddleChars(String string) {
+    if ((string.length() % 2) == 1) {
+      return Character.toString(
+        string.charAt(string.length()/2)
+      );
+    }
+    
+    double middleOfString = string.length()/2.0d;
+    println(middleOfString);
+    
+    return String.format("%c%c",
+      string.charAt((int) Math.floor(middleOfString)),
+      string.charAt((int) Math.ceil(middleOfString))
+    );
+  }
+
+
+  public static void main(String[] args) {
+    println(getMiddleChars("car"));
+    println(getMiddleChars("word"));
+  }
+}
