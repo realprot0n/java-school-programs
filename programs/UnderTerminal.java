a74c38c1b5300e327a12f9f6d8dfc001f7a75f4d
Proton <electronicballs69@gmail.com>
2025-10-07 15:59:48 -0500

    ASDFGHJ

--- programs/UnderTerminal.java
+++ programs/UnderTerminal.java
@@ -1,21 +1,26 @@
-public class TextRPG {
+import java.util.Hashtable;
+
+public class UnderTerminal {
   public static void main(String[] args) {
     
   }
 }
 
 class Item {
-  String name;
-  String description;
+  static public Hashtable<String, Item> itemNames = new Hashtable<String, Item>();
+  public String name;
+  public String description;
+  
+  
 }
 
 class Player {
-  String name;
-  int health;
-  int gold;
+  public String name;
+  public int health;
+  public int gold;
 }
 
-class Output {
+class MyOutput {
   public static void println(Object obj) {
     System.out.println(obj);
   }
