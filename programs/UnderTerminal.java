0602e72314aa009bde064aa0a18a0fb874a3f1df
Proton <electronicballs69@gmail.com>
2025-10-08 09:06:12 -0500

    lowkey dont know if i'll be able to finish this thing

--- programs/UnderTerminal.java
+++ programs/UnderTerminal.java
@@ -7,11 +7,44 @@ public class UnderTerminal {
 }
 
 class Item {
-  static public Hashtable<String, Item> itemNames = new Hashtable<String, Item>();
+  public static Hashtable<String, Item> itemNames = new Hashtable<String, Item>();
   public String name;
   public String description;
   
+  public static void initializeItemNames() {
+    itemNames.clear();
+    
+    itemNames.put("Stick", new Weapon("Stick", "Its bark is worse than its bite.", 0));
+    itemNames.put("Monster Candy", new HealingItem("Monster Candy", "Has a distinct, non-licorice flavor.", 10));
+  }
   
+  public Item(String name, String description) {
+    this.name = name;
+    this.description = description;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("%s: %s", name, description);
+  }
+}
+
+class HealingItem extends Item {
+  public int healAmount;
+
+  public HealingItem(String name, String description, int healAmount) {
+    super(name, String.format("Heals %d HP. %s", healAmount, description));
+    this.healAmount = healAmount;
+  }
+}
+
+class Weapon extends Item {
+  public int damage;
+
+  public Weapon(String name, String description, int damage) {
+    super(name, String.format("Weapon, AT %d. %s", damage, description));
+    this.damage = damage;
+  }
 }
 
 class Player {
