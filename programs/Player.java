f724cc146e3b53ab20c77842027840833407eec4
Proton <electronicballs69@gmail.com>
2025-12-01 09:09:55 -0600

    stard player.java

---
+++ programs/Player.java
@@ -0,0 +1,19 @@
+public class Player {
+  private static int numPlayers = 0;
+  private int x;
+  private int y;
+  private int z;
+  private Direction direction
+  private int health;
+  private String name;
+  
+}
+
+enum Direction {
+  North,
+  South,
+  Up,
+  Down,
+  East,
+  West
+}
