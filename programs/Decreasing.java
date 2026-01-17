abdc5c8caa8a45ced4f6aca93f3262954cf6f573
Proton <electronicballs69@gmail.com>
2026-01-17 13:12:41 -0600

    those who dont decrease

---
+++ programs/Decreasing.java
@@ -0,0 +1,14 @@
+public class Decreasing {
+  private static boolean digitsAreDecreasing(int numb) {
+    String numStr = String.valueOf(numb);
+    
+    int lastNum;
+    for (char digitChar : numStr.toCharArray()) {
+      
+    }
+  }
+  
+  public static void main(String[] args) {
+    
+  }
+}
